4f0612df86c9a356fdc13119d16042c85c8cf748
Pavel Karman <karmanpavel.pavlkan@gmail.com>
2023-02-09 21:54:35 +0300

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
-
 import { App } from './app/App';
 import { store } from './app/store/store';
 
