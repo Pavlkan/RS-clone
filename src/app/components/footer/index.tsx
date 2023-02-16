b11f5b75fc0b9c9e89c81a50e7e2e4a571f9262a
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-02-16 02:04:44 +0200

    fix: change footer text colors to prevent hiding with light theme

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Box, Link } from '@mui/material';
+import { Box, Link, useTheme } from '@mui/material';
 import dataLinks from './dataLinks';
 
 export const Footer = () => {
+  const theme = useTheme();
   const links = dataLinks.map((dataLink, key) => {
     return (
-      <Link underline="hover" href={dataLink.link} key={key} color={'Menu'} margin={'1em'}>
+      <Link underline="hover" href={dataLink.link} key={key} color={theme.palette.text.primary} margin={'1em'}>
         {dataLink.text}
       </Link>
     );
