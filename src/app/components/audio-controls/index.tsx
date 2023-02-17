9b017c1104ae3fe4ae98bcf6dd6abd76e37a098f
Rimfis <dimitri.rim@yandex.ru>
2023-02-17 10:39:56 +0300

    fix: status sound

--- src/app/components/audio-controls/index.tsx
+++ src/app/components/audio-controls/index.tsx
@@ -5,35 +5,42 @@ import VolumeOffRoundedIcon from '@mui/icons-material/VolumeOffRounded';
 import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded';
 import { IconButton } from '@mui/material';
 
-let statusAudio = true;
 const audioClick = new Audio(clickAudio);
 const audioFlip = new Audio(flipAudio);
-const soundOf = (state: boolean) => {
-  statusAudio = state;
-};
+
+function setStatusSound(status: boolean) {
+  localStorage.setItem('statusSound', `${status}`);
+}
+function getStatusSuond() {
+  const status = localStorage.getItem('statusSound');
+  if (status) {
+    return JSON.parse(status);
+  }
+  setStatusSound(true);
+}
 export const playClick = () => {
-  if (statusAudio) {
+  if (getStatusSuond()) {
     audioClick.currentTime = 0;
     audioClick.play();
   }
 };
 export const playFlip = () => {
-  if (statusAudio) {
+  if (getStatusSuond()) {
     audioFlip.currentTime = 0;
     audioFlip.play();
   }
 };
 
 const ConstrolsAudio = () => {
-  const [status, setStatus] = useState(true);
+  const [status, setStatus] = useState(getStatusSuond());
   return (
     <>
       <IconButton
         sx={{ justifySelf: 'center' }}
         onClick={() => {
+          setStatusSound(!status);
+          setStatus(!status);
           playClick();
-          setStatus(status ? false : true);
-          soundOf(status);
         }}
       >
         {status ? <VolumeUpRoundedIcon /> : <VolumeOffRoundedIcon />}
