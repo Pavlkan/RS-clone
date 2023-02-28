2e8a34b11c90474886ca55d3984cb4ccfcfc0026
Pavel Karman <karmanpavel.pavlkan@gmail.com>
2023-02-28 22:55:47 +0300

    chore: update config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-  apiUrl: process.env.REACT_APP_API_URL ?? 'http://localhost:3001',
+  apiUrl: process.env.REACT_APP_API_URL ?? 'https://gpc-api-production.up.railway.app',
 };
